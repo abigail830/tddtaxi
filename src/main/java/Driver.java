a6490f0c9708c5365fa74ee577d1c9d613ca0fca
SaraQian <abigail830@163.com>
2019-04-16 11:21:21 +0800

    step 4. refactor

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,12 +1,13 @@
 public class Driver {
 
     private Taxi taxi;
+    final double WAIT_FEE = 0.25;
 
     public Driver(Taxi taxi) {
         this.taxi = taxi;
     }
 
     public Double charge(double distance, int waitMin) {
-        return taxi.calculate(distance) + waitMin * 0.25;
+        return taxi.calculate(distance) + waitMin * WAIT_FEE;
     }
 }
--- src/test/java/DriverTest.java
+++ src/test/java/DriverTest.java
@@ -3,18 +3,21 @@ import org.junit.jupiter.api.Test;
 
 public class DriverTest {
 
-
     @Test
     void should_get_wait_charge_per_min() {
+        verifyDriverCharge(2.0, 1, 6.25);
+    }
+
+    private void verifyDriverCharge(Double distance, int waitMin, Double expectAmount) {
         //given
         final Taxi taxi = new Taxi();
         final Driver driver = new Driver(taxi);
 
         //when
-        Double charge = driver.charge(2.0, 1);
+        Double charge = driver.charge(distance, waitMin);
 
         //then
-        Assertions.assertEquals(6.25, charge);
+        Assertions.assertEquals(expectAmount, charge);
     }
 
 }
